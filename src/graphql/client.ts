39205c2a65bfed16573cfa233464030315b8312b
João Melo <jvitor.ovidio2@gmail.com>
2022-11-04 23:06:37 -0300

    remove console.log

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,7 +1,5 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-console.log(process.env.NEXT_PUBLIC_API_URI);
-
 export const apolloClient = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_API_URI,
   cache: new InMemoryCache(),
